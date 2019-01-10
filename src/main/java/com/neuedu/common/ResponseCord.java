974df5fc0ccc39f94ab37c65007f69bf66be518a
yxy <1224116199@qq.com>
2019-01-10 10:42:26 +0800

    商品模块

--- src/main/java/com/neuedu/common/ResponseCord.java
+++ src/main/java/com/neuedu/common/ResponseCord.java
@@ -40,4 +40,37 @@ public class ResponseCord {
             this.desc = desc;
         }
     }
+    //定义商品状态（枚举类型）
+    public enum ProductEnum{
+        PRODUCT_ONSTATUS(1,"在售"),
+        PRODUCT_OFFSTATUS(2,"下架"),
+        PRODUCT_DELETE(3,"删除")
+        ;
+        private Integer code;
+        private String desc;
+
+        private ProductEnum(){
+
+        }
+        private ProductEnum(Integer code,String desc){
+            this.code = code;
+            this.desc = desc;
+        }
+
+        public Integer getCode() {
+            return code;
+        }
+
+        public void setCode(Integer code) {
+            this.code = code;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+    }
 }
